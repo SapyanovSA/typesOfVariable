4e1d7cd83c63af68233e4075195bab490c6f5d3c
helloworld(1) <sapanovs04@gmail.com>
2026-01-17 18:05:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        byte bananas = 100;
+        System.out.println("Бананов - " + bananas + " кг.");
+
+        int sugar = 3;
+        float onePortion = sugar / 4;
+        System.out.println("Одна порция сахара весит " + onePortion + " кг.");
+
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        int d = a + b + c;
+        System.out.println("d = " + d);
+
+        float e = a + 1f;
+        System.out.println("e = " + e);
+
+    }
+}
